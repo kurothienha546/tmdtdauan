ae3fea06ae74545bdf667534140a902b5b08a6bf
kurothienha546 <115891604+kurothienha546@users.noreply.github.com>
2026-05-28 12:38:11 +0700

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import MoaMoa from '../MoaMoa.jsx' 
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <MoaMoa />
+  </React.StrictMode>,
+)
